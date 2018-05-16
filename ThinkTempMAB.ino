9b9707511ab6932c5ba29579d23979bdd043913b
Michael Beaudoin <michael.beaudoin@mablabs.com>
2018-05-16 16:58:59 -0500

    Add files via upload
    First upload

---
+++ ThinkTempMAB.ino
@@ -0,0 +1,202 @@
+/*
+  ThinkTempMAB.ino - sketch for esp8266 Arduino IDE
+  Modified by Micheal A Beaudoin, May 2018
+*/
+
+#include <ESP8266WiFi.h>
+#include <OneWire.h>
+
+ADC_MODE(ADC_VCC);
+
+OneWire  ds(4);  // on pin 4 (a 4.7K resistor is necessary)
+
+const int LED_PIN = 5; // Thing's onboard, green LED
+
+const char* ssid     = "BJMBEAUDOIN";
+const char* password = "bjmb2562";
+
+// api.thingspeak.com
+const char* host = "184.106.153.149";
+const char* privateKey = "EUI8192EXC4ROMF9";
+
+int rest = 900; //15 minutes in seconds
+
+void setup() {
+  pinMode(LED_PIN, OUTPUT); // Set LED pin (5) as an output
+
+  Serial.begin(115200);
+  delay(10);
+
+  // We start by connecting to a WiFi network
+
+  Serial.println();
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  
+  WiFi.begin(ssid, password);
+  
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.println("WiFi connected");  
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void loop() {
+  byte i;
+  byte present = 0;
+  byte type_s;
+  byte data[12];
+  byte addr[8];
+  float celsius;
+  float fahrenheit;
+  float vcc;
+  
+  digitalWrite(LED_PIN, HIGH); //Turn LED On
+
+  if ( !ds.search(addr)) {
+    Serial.println("No more addresses.");
+    Serial.println();
+    ds.reset_search();
+    delay(250);
+    return;
+  }
+  
+  Serial.print("ROM =");
+  for( i = 0; i < 8; i++) {
+    Serial.write(' ');
+    Serial.print(addr[i], HEX);
+  }
+
+  if (OneWire::crc8(addr, 7) != addr[7]) {
+      Serial.println("CRC is not valid!");
+      return;
+  }
+  Serial.println();
+ 
+  // the first ROM byte indicates which chip
+  switch (addr[0]) {
+    case 0x10:
+      Serial.println("  Chip = DS18S20");  // or old DS1820
+      type_s = 1;
+      break;
+    case 0x28:
+      Serial.println("  Chip = DS18B20");
+      type_s = 0;
+      break;
+    case 0x22:
+      Serial.println("  Chip = DS1822");
+      type_s = 0;
+      break;
+    default:
+      Serial.println("Device is not a DS18x20 family device.");
+      return;
+  } 
+
+  ds.reset();
+  ds.select(addr);
+  ds.write(0x44, 1);        // start conversion, with parasite power on at the end
+  
+  delay(1000);     // maybe 750ms is enough, maybe not
+  // we might do a ds.depower() here, but the reset will take care of it.
+  
+  present = ds.reset();
+  ds.select(addr);    
+  ds.write(0xBE);         // Read Scratchpad
+
+  Serial.print("  Data = ");
+  Serial.print(present, HEX);
+  Serial.print(" ");
+  for ( i = 0; i < 9; i++) {           // we need 9 bytes
+    data[i] = ds.read();
+    Serial.print(data[i], HEX);
+    Serial.print(" ");
+  }
+  Serial.print(" CRC=");
+  Serial.print(OneWire::crc8(data, 8), HEX);
+  Serial.println();
+
+  // Convert the data to actual temperature
+  // because the result is a 16 bit signed integer, it should
+  // be stored to an "int16_t" type, which is always 16 bits
+  // even when compiled on a 32 bit processor.
+  int16_t raw = (data[1] << 8) | data[0];
+  if (type_s) {
+    raw = raw << 3; // 9 bit resolution default
+    if (data[7] == 0x10) {
+      // "count remain" gives full 12 bit resolution
+      raw = (raw & 0xFFF0) + 12 - data[6];
+    }
+  } else {
+    byte cfg = (data[4] & 0x60);
+    // at lower res, the low bits are undefined, so let's zero them
+    if (cfg == 0x00) raw = raw & ~7;  // 9 bit resolution, 93.75 ms
+    else if (cfg == 0x20) raw = raw & ~3; // 10 bit res, 187.5 ms
+    else if (cfg == 0x40) raw = raw & ~1; // 11 bit res, 375 ms
+    //// default is 12 bit resolution, 750 ms conversion time
+  }
+  celsius = (float)raw / 16.0;
+  fahrenheit = celsius * 1.8 + 32.0;
+  vcc = (ESP.getVcc() / 1000.0);
+  
+  Serial.print("  Temperature = ");
+  Serial.print(fahrenheit);
+  Serial.print(" Fahrenheit, ");
+//  Serial.print(celsius);
+//  Serial.print(" Celsius, ");
+
+  // got temp, now publish it
+  Serial.print("connecting to ");
+  Serial.println(host);
+  
+  // Use WiFiClient class to create TCP connections
+  WiFiClient client;
+  const int httpPort = 80;
+  if (!client.connect(host, httpPort)) {
+    Serial.println("connection failed");
+    return;
+  }
+
+
+  // We now create a URI for the request
+  String url = "/update?key=";
+  url += privateKey;
+  url += "&field1=";
+  url += fahrenheit;      // cast from float to string I guess
+  url += "&field2=";
+  url += vcc;      // cast from float to string I guess
+//  url += celsius;      // cast from float to string I guess
+  
+  Serial.print("Requesting URL: ");
+  Serial.println(url);
+  
+  // This will send the request to the server
+  client.print(String("GET ") + url + " HTTP/1.1\r\n" +
+               "Host: " + host + "\r\n" + 
+               "Connection: close\r\n\r\n");
+  delay(5);
+  
+
+  // Read all the lines of the reply from server and print them to Serial
+  while(client.available()){
+    String line = client.readStringUntil('\r');
+    Serial.print(line);
+  }
+  
+  Serial.println();
+  Serial.println("closing connection");
+  
+  // This was missing in the WiFiClient example - would stop after 4 data points
+  client.stop();
+  
+  digitalWrite(LED_PIN, LOW); //Turn LED Off
+  // update every 5 minutes (300000)
+  //delay(300000);
+  ESP.deepSleep((rest * 1000000), WAKE_RF_DEFAULT); // Sleep for rest seconds
+}
+
